c6b7be336a93ef588018d89bcefc54cc096284cf
Roman Kvitkov <rk@316.su>
2020-10-11 14:32:12 +0500

    Initial data seed
    + BadOperation exception

--- database/migrations/2020_10_11_090048_create_houses_table.php
+++ database/migrations/2020_10_11_090048_create_houses_table.php
@@ -24,7 +24,7 @@ class CreateHousesTable extends Migration
                 $table->smallInteger('bathrooms')->index();
                 $table->smallInteger('storeys')->index();
                 $table->smallInteger('garages')->index();
-                $table->timestampsTz();
+                $table->timestamps();
             }
         );
     }
